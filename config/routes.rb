b308d961210e017917a7981a79c526ab0485619d
gvterechov <123freedom123@mail.ru>
2021-02-22 00:28:41 +0300

    Внешний вид страниц

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
+  get :task, to: 'application#task'
+
+  scope ":locale", locale: /#{I18n.available_locales.join('|')}/ do
+    root 'application#index', as: :root_with_locale
+    get :task, to: 'application#task'
+  end
 end
