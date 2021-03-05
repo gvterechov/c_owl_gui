48991cc39ddc3cdeec1febdd3b5483fc37e501d8
den1s0v <hello-lumen@ya.ru>
2021-03-05 04:50:13 +0400

    Исправлена передача JSON из hidden в c_owl/verify_trace_act

--- app/services/c_owl.rb
+++ app/services/c_owl.rb
@@ -10,7 +10,7 @@ class COwl
   def self.verify_trace_act(expression = nil)
     uri = URI.parse("http://localhost:2020/verify_trace_act")
     http = Net::HTTP.new(uri.host, uri.port)
-    response = http.post(uri, expression.to_json)
+    response = http.post(uri, expression.to_json, 'Content-Type': 'application/json')
     JSON.load(response.body)&.with_indifferent_access
   end
 end
